085d8aedff74b6a2a45d51b32b4d12bc11d7f205
MitThakkar24 <108894022+MitThakkar24@users.noreply.github.com>
2023-11-15 20:47:50 +0530

    Added Database Connection

--- Flask/FlaskWebApplication/app.py
+++ Flask/FlaskWebApplication/app.py
@@ -1,21 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
+from datetime import datetime
 
-@app.route('/')
-def Home():
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/blogwebdatabase'
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+
+@app.route("/")
+def home():
     return render_template('index.html')
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contact(name=name, phone_num=phone, msg=message, date=datetime.now(), email=email)
+
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('contact.html')
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
 
 app.run(debug=True)
